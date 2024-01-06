c44594f1e2d41fa50e523ccf7c94d526a5356eb8
Arad Ben Menashe <50054061+aradbm@users.noreply.github.com>
2024-01-06 11:09:14 +0200

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#if defined (KEYBOARD_crkbd)
+#if defined (KEYBOARD_piantor)
 #define MIRYOKU_MAPPING( \
       K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
       K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
