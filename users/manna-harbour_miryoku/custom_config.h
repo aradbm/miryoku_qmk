8a79ae1b22db2f6f89ee2a66c567728dd134d63d
Arad Ben Menashe <50054061+aradbm@users.noreply.github.com>
2024-01-13 13:44:54 +0200

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -13,18 +13,18 @@
       N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
 ) \
 LAYOUT_split_3x6_3( \
-KC_ESC,           K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  KC_SLASH, \
-KC_TAB,          K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  KC_MINS, \
-LSFT_T(KC_CAPS),  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  CW_TOGG, \
+KC_LCTL,          K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,      KC_SLASH, \
+KC_LALT,          K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,      KC_MINS, \
+KC_LGUI,          K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,      LSFT_T(KC_CAPS), \
                               K32,  K33,  K34,         K35,  K36,  K37 \
 )
 
 
 #define MIRYOKU_LAYER_BASE \
-KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              LSFT(KC_1),           \
+KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              LSFT(KC_1),        \
 LCTL_T(KC_A),      LALT_T(KC_R),      LGUI_T(KC_S),      LSFT_T(KC_T),      KC_G,              KC_M,              LSFT_T(KC_N),      LGUI_T(KC_E),      LALT_T(KC_I),      LCTL_T(KC_O),      \
-LT(U_BUTTON,KC_Z), KC_X,              KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           KC_DOT,            LT(U_BUTTON,KC_SCLN),\
-U_NP,              U_NP,              LT(U_MEDIA,KC_ESC), KC_TAB, LT(U_NAV,KC_SPC),            LT(U_NUM,KC_BSPC),  LT(U_SYM,KC_ENT), LT(U_FUN,KC_DEL),           U_NP,              U_NP
+KC_Z,              KC_X,              KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           KC_DOT,            KC_SCLN,           \
+U_NP,              U_NP,              LT(U_MEDIA,KC_ESC),LT(U_SYM,KC_TAB),  LT(U_NAV,KC_SPC),  LT(U_NUM,KC_BSPC), LT(U_SYM,KC_ENT),  LT(U_FUN,KC_DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 KC_QUOTE,        LSFT(KC_COMMA),        LSFT(KC_DOT),          LSFT(KC_QUOTE),        LSFT(KC_DOT),        LSFT(KC_7),                  U_NA,                         KC_LEFT_BRACKET,                    KC_RIGHT_BRACKET,                  LSFT(KC_5),     \
@@ -40,7 +40,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #define MIRYOKU_LAYER_NUM \
 KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,           U_NA,              TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
-KC_SCLN,           KC_4,              KC_5,              KC_6,              KC_EQL,            U_NA,             KC_LSFT,           KC_LGUI,           KC_LALT,           KC_LCTL,            \
+KC_SCLN,           KC_4,              KC_5,              KC_6,              KC_EQL,            U_NA,              KC_LSFT,           KC_LGUI,           KC_LALT,           KC_LCTL,            \
 KC_GRV,            KC_1,              KC_2,              KC_3,              KC_BSLS,           U_NA,              TD(U_TD_U_NUM),    TD(U_TD_U_NAV),    KC_ALGR,           U_NA,              \
 U_NP,              U_NP,              KC_DOT,            KC_0,              KC_MINS,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
